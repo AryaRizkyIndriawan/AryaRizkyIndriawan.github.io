30e8e0c80958e3e0db0a44f5458ac8f2722e4011
Arya Rizky Indriawan <155818175+AryaRizkyIndriawan@users.noreply.github.com>
2024-02-16 07:51:31 +0700

    Merubah menjadi responsif

--- ubah.php
+++ ubah.php
@@ -35,6 +35,22 @@ if (isset($_POST["submit"])) {
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+ <!-- Additional styles for responsiveness -->
+ <style>
+        @media (max-width: 576px) {
+            .form-label {
+                text-align: left;
+            }
+        }
+
+        @media (max-width: 768px) {
+            .btn-responsive {
+                display: block;
+                width: 100%;
+                margin-bottom: 0.5rem;
+            }
+        }
+    </style>
 </head>
 
 <body>
@@ -70,7 +86,7 @@ if (isset($_POST["submit"])) {
   </nav> -->
   <div class="container my-3">
     <div class="row">
-      <div class="col-8">
+      <div class="col-lg-8 mx-auto">
         <h2 class="my-3">Edit data mahasiswa</h2>
         <form method="post" autocomplete="off" enctype="multipart/form-data">
           <input type="hidden" name="id_mhs" value="<?= $mhs["id_mhs"]; ?>">
@@ -151,8 +167,8 @@ if (isset($_POST["submit"])) {
               </div>
             </div>
           </div>
-          <button type="submit" class="btn btn-primary" name="submit"><i class="fa-solid fa-file-pen"></i> Edit Data</button>
-          <a class="btn btn-warning" href="pages/UI/data.php" role="button"><i class="fa-solid fa-circle-left"></i> Back</a>
+          <button type="submit" class="btn btn-primary btn-responsive" name="submit"><i class="fa-solid fa-file-pen"></i> Edit Data</button>
+          <a class="btn btn-warning btn-responsive" href="pages/UI/data.php" role="button"><i class="fa-solid fa-circle-left"></i> Back</a>
         </form>
       </div>
     </div>
